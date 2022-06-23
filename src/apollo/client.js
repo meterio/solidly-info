90c7e79e5d742daa16851398805e71d8b1dab7e1
philipappiah <pappiahkubi1@gmail.com>
2022-06-23 10:04:04 +0100

    initial commit

---
+++ src/apollo/client.js
@@ -0,0 +1,37 @@
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+
+export const client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://graph-on-testnet.meter.io/subgraphs/name/meterio/solidly-subgraph'
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true
+})
+
+export const healthClient = new ApolloClient({
+  link: new HttpLink({
+    uri: 'http://18.138.233.236:8030/graphql'
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true
+})
+
+export const v1Client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://graph-on-testnet.meter.io/subgraphs/name/meterio/solidly-subgraph'
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true
+})
+
+//block client will be updated to http://18.141.138.134:8000/subgraphs/name/meter/blocks after an appreciable blocks sync
+export const blockClient = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://graph-on-testnet.meter.io/subgraphs/name/blocklytics/meter-solidly-blocks'
+  }),
+  cache: new InMemoryCache()
+})
+
+
